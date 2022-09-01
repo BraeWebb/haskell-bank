9a3d5a09fa388d1d1606c32c784e5059e74dd958
Brae Webb <email@braewebb.com>
2022-09-01 18:57:07 +1000

    Trial hspec instead

---
+++ Q001/Spec.hs
@@ -0,0 +1,13 @@
+import Main (double, half)
+import Test.Hspec
+import Test.QuickCheck
+
+main :: IO ()
+main = hspec $ do
+  describe "Double and Half" $ do
+    it "double of 4 is 8" $ do
+      double 4 `shouldBe` (8 :: Int)
+
+    it "half of 9 is 4" $
+      half 9 `shouldBe` (4 :: Int)
+
